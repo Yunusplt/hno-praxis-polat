82fb22bb8b9997084750239a763ab852d99ebe41
yunus <yunus.polat@simdatix.com>
2024-10-21 12:18:40 +0200

    remove data fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import Header from "@/components/Header";
 import type { Metadata } from "next";
 import "./global.css";
 
+// Amplify configuration
+import { Amplify } from "aws-amplify";
+import outputs from "../../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
 export const metadata: Metadata = {
   title: "HNO Praxis Esslingen | Dr. Bahtiyar Polat",
   description: "HNO Praxis Esslingen | Dr. Bahtiyar Polat",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,7 @@ import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import HomePageImage from "@/assets/images/HomePageImage.jpg";
 
-async function getData() {
-  const response = await fetch(process.env.BASE_URL + "/api/appointments");
-  const appointments = await response.json();
-  console.log(appointments);
-  return appointments;
-}
-
 export default async function Home() {
-  const appointments = await getData();
   return (
     <main>
       <Box
@@ -34,9 +26,6 @@ export default async function Home() {
       <Box>
         <Typography variant="h1" align="center" height="100vh">
           HNO Praxis Esslingen | Dr. Bahtiyar Polat
-        </Typography>
-        <Typography variant="h2" align="center">
-          {JSON.stringify(appointments)}
         </Typography>
       </Box>
     </main>
