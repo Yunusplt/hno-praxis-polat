9212735e8f835542805624e5e667e4068149bdf6
yunus <yunus.polat@simdatix.com>
2025-01-27 17:23:36 +0100

    design neue Service page

---
+++ src/features/services/utils.ts
@@ -0,0 +1,114 @@
+import Allgemein from "@/assets/images/services/Allgemeine Untersuchung.jpg";
+import Otoakusticher from "@/assets/images/services/otoakusticher.jpg";
+import LOGO from "@/assets/images/HNO_Polat_Color.png";
+import Pricktest from "@/assets/images/services/Pricktest.jpg";
+import Hörtestung from "@/assets/images/services/Hörtestung.jpg";
+import Tympanometrie from "@/assets/images/services/Tympanometrie.jpg";
+import Endoskopie from "@/assets/images/services/Endoskopische Untersuchung.jpg";
+import Mikroskopische from "@/assets/images/services/Mikroskopische Untersuchung.jpg";
+
+export const serviceData1 = [
+  {
+    title: "Pricktest",
+    src: Pricktest,
+    alt: "Pricktest",
+  },
+  {
+    title: "Allergologie",
+    src: LOGO,
+    alt: "Allergologie",
+  },
+  {
+    title: "Bluttest (RAST)",
+    src: LOGO,
+    alt: "Bluttest (RAST)",
+  },
+  {
+    title: "Hörstörungen",
+    src: LOGO,
+    alt: "Hörstörungen",
+  },
+  {
+    title: "Hörtestung (Audiometrie)",
+    src: Hörtestung,
+    alt: "Hörtestung",
+  },
+  {
+    title: "Sprachtestung",
+    src: LOGO,
+    alt: "Sprachtestung",
+  },
+  {
+    title: "Tonaudiometrie",
+    src: LOGO,
+    alt: "Tonaudiometrie",
+  },
+  {
+    title: "Tympanometrie",
+    src: Tympanometrie,
+    alt: "Tympanometrie",
+  },
+  {
+    title: "Kopfimpulstest (HIT)",
+    src: LOGO,
+    alt: "Kopfimpulstest",
+  },
+  {
+    title: "Lupenlaryngoskopie",
+    src: LOGO,
+    alt: "Lupenlaryngoskopie",
+  },
+
+  {
+    title: "Ultraschalluntersuchungen",
+    src: LOGO,
+    alt: "Ultraschalluntersuchungen",
+  },
+];
+
+export const serviceData2 = [
+  {
+    title: "Endoskopische Untersuchung",
+    src: Endoskopie,
+    alt: "Endoskopische-Untersuchung",
+  },
+  {
+    title: "Mikroskopische Untersuchung",
+    src: Mikroskopische,
+    alt: "Mikroskopische-Untersuchung",
+  },
+  {
+    title: "Allgemeine HNO-ärtzliche Untersuchung",
+    src: Allgemein,
+    alt: "Allgemein Untersuchungen",
+  },
+  {
+    title: "Otoakusticher Emissionstest (OAE)",
+    src: Otoakusticher,
+    alt: "Otoakusticher Emissionstest",
+  },
+
+  {
+    title: "Schwindeldiagnostik (Vestibularisdiagnostik)",
+    src: LOGO,
+    alt: "Schwindeldiagnostik",
+  },
+
+  {
+    title: "Hörgeräteverordnung und -überprüfung",
+    src: LOGO,
+    alt: "Hörgeräteverordnung",
+  },
+
+  {
+    title: "Neugeborenen-Hörscreening",
+    src: LOGO,
+    alt: "Neugeborenen-Hörscreening",
+  },
+
+  {
+    title: "Abklärung von Sprachstörungen, Sprache und Stimme",
+    src: LOGO,
+    alt: "Abklärung-von-Sprachstörungen",
+  },
+];
